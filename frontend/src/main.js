36d0f75eeabe59d5693f37355c7b312c2e811ce9
Potatomochi <noahrsam@gmail.com>
2021-11-14 04:22:02 +0800

    fMostUpdated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+import VTypical from 'vue-typical'
+
+
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
+const app = createApp(App);
+
+app.config.productionTip = false
+app.use(router).use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyAjnoiL_PASCr0AG9l4LU00PUokxfFC28A',
+        libraries : ["places" , "visualization"]
+    },
+    autobindAllEvents: true,
+}).use(VTypical).mount('#app')
+// createApp(App).use(router).mount('#app')
+
